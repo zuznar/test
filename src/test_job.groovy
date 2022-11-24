73f1d292c356cd9de16ecd7446e6613a065c4331
SG0316839 <Zuzanna.Narolska@sabre.com>
2022-11-24 17:08:20 +0100

    update test credentials job pipeline and credntials backup

--- src/test_job.groovy
+++ src/test_job.groovy
@@ -3,11 +3,47 @@ node('master') {
     stage('Pre-commit clean') {
         cleanWs()
     }
-    stage('Git') {
+    stage('test-http-token') {
         echo 'Cloning configuration'
-        git branch: 'master', url: 'https://proxy.git.sabre-gcp.com/scm/sswops/zuzanna-tests.git', credentialsId: 'svc-ark-ctopscicd-nexus-token-dev'
+        git branch: 'master', url: 'https://proxy.git.sabre-gcp.com/scm/sswops/zuzanna-tests.git', credentialsId: 'svc-ark-sswdcdevgcp-git-http-token'
     }
+
+    stage('test-ssh-key') {
+        echo 'Cloning configuration'
+        git branch: 'master', url: 'ssh://git@proxy.git.sabre-gcp.com/sswops/zuzanna-tests.git', credentialsId: 'svc-ark-sswdcdevgcp-git-ssh-key'
+    }
+
     stage('Post-commit clean') {
         cleanWs()
+    }
+
+    stage("download from nexus trusted dev ctops") {
+        response = httpRequest url: "https://gcp.repository.sabre.com/repository/config-trusted" + "/" + "com/sabre/as/ecomm/bundles/stacks/awsdev" + "/" +
+                "cf-as-dev-ecomm-jenkins88m" + "/" + "2.0.361" + "/" +
+                "cf-as-dev-ecomm-jenkins88m" + "-" + "2.0.361" + ".tar.gz",
+                outputFile: "cf-as-dev-ecomm-jenkins88m-ctops" + "-" + "2.0.361" + ".tar.gz" ,
+                authentication: "svc-ark-ctopscicd-nexus-token-dev"
+    }
+
+    stage("download from nexus trusted dev sswdev") {
+        response = httpRequest url: "https://gcp.repository.sabre.com/repository/config-trusted" + "/" + "com/sabre/as/ecomm/bundles/stacks/awsdev" + "/" +
+                "cf-as-dev-ecomm-jenkins88m" + "/" + "2.0.360" + "/" +
+                "cf-as-dev-ecomm-jenkins88m" + "-" + "2.0.360" + ".tar.gz",
+                outputFile: "cf-as-dev-ecomm-jenkins88m-sswdev" + "-" + "2.0.360" + ".tar.gz" ,
+                authentication: "svc-ark-sswdcdevgcp-nexus-token-dev"
+    }
+
+    stage("promote to trusted") {
+        triggerRemoteJob(
+                remoteJenkinsName: "gcp.promote.repository.sabre.com",
+                auth: CredentialsAuth(credentials: 'svc-ark-sswdcdevgcp-promotion-token'),
+                enhancedLogging: true,
+                job: "promote-config",
+                blockBuildUntilComplete: true,
+                parameters: "groupId=" + "testgroup" + "\n" +
+                        "artifactId=" + "testartifact" + "\n" +
+                        "version=" + "testver"
+
+        )
     }
 }
